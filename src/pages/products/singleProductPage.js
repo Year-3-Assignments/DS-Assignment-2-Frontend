d2c518eb5f52990155796dd4f798ef688b3f017d
Rusiru Abhisheak <rusiruavb98@gmail.com>
2021-05-07 19:03:18 +0530

    Create landing page

--- src/pages/products/singleProductPage.js
+++ src/pages/products/singleProductPage.js
@@ -29,7 +29,8 @@ class SingleProductPage extends React.Component {
       product: '',
       shopDetails: '',
       sellerDetails: '',
-      shopProducts: ''
+      shopProducts: '',
+      isPageLoading: true
     }
   }
 
@@ -44,7 +45,8 @@ class SingleProductPage extends React.Component {
         product: nextProps.getProduct[0],
         shopDetails: nextProps.getProduct[1],
         sellerDetails: nextProps.getProduct[1].user,
-        shopProducts: nextProps.getProduct[1].products
+        shopProducts: nextProps.getProduct[1].products,
+        isPageLoading: nextProps.isProductPending
       });
     }
   }
@@ -52,58 +54,71 @@ class SingleProductPage extends React.Component {
   render() {
     let { product, shopDetails, sellerDetails, shopProducts } = this.state;
     return (
-      <div className="container" style={{marginTop: '80px'}}>
-        <div className="row">
-          <div className="col-md-5">
-            <img src={product.imageUrl} className="single-product-image" alt="product-image" />
+      <div>
+        {!this.state.isPageLoading ? 
+          <div className="container" style={{marginTop: '80px'}}>
+            <div className="row">
+              <div className="col-md-5">
+                <img src={product.imageUrl} className="single-product-image" alt="product-image" />
+              </div>
+
+              <div className="col-md-7">
+                <h1 className="product-title">{product.productName}</h1>
+                <h4>Product Information</h4>
+                <p className="product-info-text m-0"><b>Item Code</b> : {product.itemCode}</p>
+                <p className="product-info-text m-0"><b>Unit Price</b> : LKR {product.unitPrice}.00</p>
+                <p className="product-info-text m-0"><b>Quantity</b> : {product.quantity}</p>
+                <p className="product-info-text m-0"><b>Desciption</b> :</p>
+                <p className="product-info-text m-0 pb-3">{product.description}</p>
+
+                <h4>Shop Details</h4>
+                <p className="product-info-text m-0"><b>Shop Name</b> : {shopDetails.shopName}</p>
+                <p className="product-info-text m-0"><b>Phone</b> : {shopDetails.phoneNumber}</p>
+                <p className="product-info-text m-0"><b>Seller</b> : {sellerDetails.firstName}&nbsp;{sellerDetails.lastName}</p>
+                <p className="product-info-text m-0"><b>Email</b> : {sellerDetails.email}</p>
+                <p className="product-info-text m-0 pb-3"><b>Address</b> : {sellerDetails.address_1}, {sellerDetails.address_2}, {sellerDetails.city}</p>
+
+                <button className="btn btn-dark btn-pill"><b>ADD TO CART</b>&nbsp;&nbsp;<i className="fas fa-cart-arrow-down"></i></button>&nbsp;&nbsp;
+                <button className="btn btn-dark btn-pill"><b>CONTACT SELLER</b>&nbsp;&nbsp;<i className="fas fa-phone-volume"></i></button>
+              </div>
+            </div>
+
+            <div className="row mt-5">
+              <h4 className="mb-3">Products from <b>{shopDetails.shopName}</b></h4>
+              {shopProducts.length && (
+                <OwlCarousel items={6} className="owl-theme" loop nav margin={10} responsive={responsive}>
+                  {shopProducts.map((product) => (
+                    <div className="item">
+                      <SingleShopProduct key={product.id} 
+                        imageUrl={product.imageUrl}
+                        productName={product.productName} 
+                        itemCode={product.itemCode} 
+                        quantity={product.quantity} 
+                        unitPrice={product.unitPrice} 
+                      />
+                    </div>
+                  ))}
+                </OwlCarousel>
+              )}
+            </div>
           </div>
-
-          <div className="col-md-7">
-            <h1 className="product-title">{product.productName}</h1>
-            <h4>Product Information</h4>
-            <p className="product-info-text m-0"><b>Item Code</b> : {product.itemCode}</p>
-            <p className="product-info-text m-0"><b>Unit Price</b> : LKR {product.unitPrice}.00</p>
-            <p className="product-info-text m-0"><b>Quantity</b> : {product.quantity}</p>
-            <p className="product-info-text m-0"><b>Desciption</b> :</p>
-            <p className="product-info-text m-0 pb-3">{product.description}</p>
-
-            <h4>Shop Details</h4>
-            <p className="product-info-text m-0"><b>Shop Name</b> : {shopDetails.shopName}</p>
-            <p className="product-info-text m-0"><b>Phone</b> : {shopDetails.phoneNumber}</p>
-            <p className="product-info-text m-0"><b>Seller</b> : {sellerDetails.firstName}&nbsp;{sellerDetails.lastName}</p>
-            <p className="product-info-text m-0"><b>Email</b> : {sellerDetails.email}</p>
-            <p className="product-info-text m-0 pb-3"><b>Address</b> : {sellerDetails.address_1}, {sellerDetails.address_2}, {sellerDetails.city}</p>
-
-            <button className="btn btn-dark btn-pill"><b>ADD TO CART</b>&nbsp;&nbsp;<i className="fas fa-cart-arrow-down"></i></button>&nbsp;&nbsp;
-            <button className="btn btn-dark btn-pill"><b>CONTACT SELLER</b>&nbsp;&nbsp;<i className="fas fa-phone-volume"></i></button>
+        :
+          <div class="text-center page-loading">
+            <div className="align-text-bottom">
+              <div class="spinner-border" role="status" style={{width: '5rem', height: '5rem'}}>
+                <span class="visually-hidden">Loading...</span>
+              </div>
+            </div>
           </div>
-        </div>
-
-        <div className="row mt-5">
-          <h4 className="mb-3">Products from <b>{shopDetails.shopName}</b></h4>
-          {shopProducts.length && (
-            <OwlCarousel items={6} className="owl-theme" loop nav margin={10} responsive={responsive}>
-              {shopProducts.map((product) => (
-                <div className="item">
-                  <SingleShopProduct key={product.id} 
-                    imageUrl={product.imageUrl}
-                    productName={product.productName} 
-                    itemCode={product.itemCode} 
-                    quantity={product.quantity} 
-                    unitPrice={product.unitPrice} 
-                  />
-                </div>
-              ))}
-            </OwlCarousel>
-          )}
-        </div>
+        }
       </div>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  getProduct: state.productReducer.getProduct
+  getProduct: state.productReducer.getProduct,
+  isProductPending: state.productReducer.loading
 });
 
 const mapDispatchToProps = dispatch => ({
